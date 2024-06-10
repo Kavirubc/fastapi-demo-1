0d6f8ce330473a3ef213587e94df1bbf56752b46
kaviru <hapuarachchikaviru@gmail.com>
2024-06-10 14:02:41 +0530

    Path and query parameters together

--- myapi.py
+++ myapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 #Creating an instance of FastAPI
 app = FastAPI()
@@ -8,6 +9,11 @@ students = {
         "name": "jhon",
         "age": 17,
         "class": "year 12"
+    },
+    2: {
+        "name": "khan",
+        "age": 18,
+        "class": "year 15"
     }
 }
 
@@ -17,10 +23,19 @@ students = {
 def index():
     return {"Name": "First Data"}
 
+#Path parameters
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(description="Input the Id of the student", gt=0, lt=3)):
     return students[student_id]
     #inorder to get only the name or something like that
     # return {"name" : students[student_id]["name"]}
 
+#Query parameters
+
+@app.get("/get-by-name/{student_id}")
+def get_student(*,student_id: int,name : Optional[str] = None, test :int = None):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data" : "Not Found"}
 
